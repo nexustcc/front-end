daa12cbc14bfc2ee0284e16e82afd18ec77b455e
Cauã Menezes <79976791+cauamenezes@users.noreply.github.com>
2022-03-16 11:02:18 -0700

    organizando pastas

---
+++ login/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const imgExibir = document.getElementById("show_password");
+const inputSenha = document.getElementById("inputSenha");
+
+function alternarSenha() {
+    
+    if (inputSenha.type === "password") {
+      inputSenha.type = "text";
+      imgExibir.src = 'img/eye-off.svg';
+    } else {
+      inputSenha.type = "password";
+      imgExibir.src = 'img/eye.svg';
+    }
+
+  }
