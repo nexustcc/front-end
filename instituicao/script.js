fb94e0ac1c86ba88d138912e945a7b4fb830e8cd
Ana150 <anaoliveira4267@gmail.com>
2022-03-21 11:26:38 -0300

    21/03

--- instituicao/script.js
+++ instituicao/script.js
@@ -10,21 +10,7 @@ function exitModal() {
     document.querySelector(".bg").style.display = "none";
 }
 
-function showOtherModal() {
-    document.querySelector(".otherModal").style.display = "flex";
-    document.querySelector(".modal").style.display = "none";
-    document.querySelector(".bg").style.display = "flex";
-}
-
-function exitOtherModal() {
-    document.querySelector(".otherModal").style.display = "none";
-    document.querySelector(".modal").style.display = "none";
-    document.querySelector(".bg").style.display = "none";
-}
 
 
 document.getElementById("terceiro_botao")
     .addEventListener("click", showModal);
-
-document.getElementById("segundo_botao_modal")
-    .addEventListener("click", showOtherModal);
---
+++ instituicao/usuarios/cadastro/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function showModal() {
+    document.querySelector(".modal").style.display = "flex";
+    document.querySelector(".bg").style.display = "flex";
+}
+
+function exitModal() {
+    document.querySelector(".modal").style.display = "none";
+    document.querySelector(".bg").style.display = "none";
+}
+
