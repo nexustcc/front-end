3b3fb964971e9357a083b908e105dabbb3983209
Artur Duarte <arturduartemoraes@gmail.com>
2022-04-04 09:59:10 -0300

    Adicionado Listagem de Cursos

--- instituicao/cursos/script.js
+++ instituicao/cursos/script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 function showModal() {
     document.querySelector(".modal").style.display = "flex";
@@ -30,12 +30,61 @@ function exitModalCurso() {
     document.querySelector(".modal-curso").style.display = "none";
 }
 
-function showModalExcluir() {
+function showModalExcluir(idCurso) {
     document.querySelector(".bg").style.display = "flex";
     document.querySelector(".modal-excluir").style.display = "flex";
 }
 
-function exitModalExcluir() {
+function exitModalExcluir(idCurso) {
     document.querySelector(".bg").style.display = "none";
     document.querySelector(".modal-excluir").style.display = "none";
 }
+
+function exibirDados(cursos) {
+    const container = document.getElementById("ul_container");
+
+    for (var i = 0; i < cursos.length; i++) {
+        var cursosJson = {
+            idCurso: cursos[i].idCurso,
+            nome: cursos[i].nome,
+        };
+
+        console.log(cursosJson);
+
+        const curso = document.createElement("li");
+        curso.classList = "list-group-item li";
+        curso.id = "li";
+
+        curso.innerHTML += `
+        <div id="icone">
+            <p class="curso">Curso:</p>
+            <img id="book" src="./img/books.svg" alt="book" />
+            <p class="nome">${cursos[i].nome}</p>
+        </div>
+        <div class="button">
+            <button id="terceiro_botao">TURMAS EXISTENTES NO CURSO</button>
+            <button onclick="showModalEditar()" class="terceiro" id="primeiro_botao">
+                EDITAR
+            </button>
+            <button onclick="showModalExcluir()" id="quarto_botao">
+                EXCLUIR
+            </button>
+        </div>
+        `;
+
+        container.appendChild(curso);
+    }
+}
+
+async function getArrayCursos() {
+    const url = `http://localhost:3000/curso/listarCursos/3`;
+
+    fetch(url)
+        .then((response) => response.json)
+        .then(console.log);
+    const dados = await fetch(url);
+    const cursos = await dados.json();
+    exibirDados(cursos.cursos);
+}
+
+window.onload = getArrayCursos();
