b2e8f78f8d389d72e6e8de50c4b5319d92a05ea5
älexgomesz <79976791+cauamenezes@users.noreply.github.com>
2022-04-12 10:49:20 -0700

    edição de perfil do professor finalizada

---
+++ professor/perfil/editar/fotoPerfil.js
@@ -0,0 +1,22 @@
+'use strict'
+
+Array.prototype.forEach.call(document.querySelectorAll('.btn_upload_foto'), function (button) {
+
+    const hiddenInput = button.parentElement.querySelector('.input_foto');
+    let imgProfessor = document.querySelector('#fotoPerfilProfessor');
+    let uploadedImage = '';
+
+    button.addEventListener('click', function () {
+        hiddenInput.click();
+    });
+
+    hiddenInput.addEventListener('change', function() {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+            uploadedImage = reader.result;
+            imgProfessor.style.backgroundImage = `url(${uploadedImage})`;
+        })
+        reader.readAsDataURL(this.files[0]);
+    })
+});
+
