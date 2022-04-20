07d499fff8ba2ad7a3b3a289affb67a58c00b82d
älexgomesz <79976791+cauamenezes@users.noreply.github.com>
2022-04-20 12:29:09 -0700

    fazendo modais de crud de grupo

--- professor/cursos/turmas/scripts.js
+++ professor/cursos/turmas/scripts.js
@@ -16,13 +16,12 @@ function exitModal() {
 function showModalGrupos() {
     document.querySelector(".bg").style.display = "flex";
     document.querySelector(".modal-grupos").style.display = "flex";
-    document.querySelector(".modal-add-membros").style.display = "none";
 }
 
 function exitModalGrupos() {
     document.querySelector(".bg").style.display = "none";
     document.querySelector(".modal-grupos").style.display = "none";
-    document.querySelector(".modal-add-membros").style.display = "none";
+    document.querySelector(".modal-add-membros").style.display = "flex";
 }
 
 function showModalTurma() {
@@ -34,23 +33,38 @@ function showModalTurma() {
 function exitModalTurma() {
     document.querySelector(".bg").style.display = "none";
     document.querySelector(".modal-turma").style.display = "none";
-    document.querySelector(".modal-turma").style.display = "none";
-    document.querySelector(".modal-add-membros").style.display = "none";
 }
 
 function showModalMembros() {
     document.querySelector(".bg").style.display = "flex";
     document.querySelector(".modal-turma").style.display = "none";
     document.querySelector(".modal-membros").style.display = "flex";
-    document.querySelector(".modal-add-membros-turma-criada").style.display = "none"
-}
-
-function showModalEditar() {
-    document.querySelector(".bg").style.display = "flex";
-    document.querySelector(".modal-turma").style.display = "none";
-    document.querySelector(".modal-editar").style.display = "flex";
 }
 
 function resetForm() {
     document.getElementById("modal").reset();
+}
+
+const showModalCriarGrupo = () => {
+    document.querySelector(".bg").style.display = "flex";
+    document.querySelector(".modal-criar-grupo").style.display = "flex";
+    document.querySelector(".modal-membros").style.display = "none";
+}
+
+const exitModalCriarGrupo = () => {
+    document.querySelector(".modal-criar-grupo").style.display = "none";
+}
+
+function showModalExcluirGrupo() {
+    document.querySelector(".modal-grupos").style.display = "none";
+    document.querySelector(".modal-excluir-grupo").style.display = "flex";   
+}
+
+function exitModalExcluirGrupo() { 
+    document.querySelector(".modal-excluir-grupo").style.display = "none";
+    document.querySelector(".modal-grupos").style.display = "flex";
+}
+
+const excluirGrupo = (idGrupo) => {
+    console.log('excluir grupo')
 }
