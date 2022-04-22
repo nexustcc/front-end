7e2941ced8c2fd6f3c66d768f25d050e4961b635
Cauã Menezes <79976791+cauamenezes@users.noreply.github.com>
2022-04-22 19:44:17 -0300

    adicionando side menu e header nas páginas de professor, crud de usuário por parte do professor feito

--- professor/usuarios/script.js
+++ professor/usuarios/script.js
@@ -57,24 +57,85 @@ function exitModalTrocarTurma() {
     document.querySelector(".modal-membros").style.display = "flex";
 }
 
+function showModalExcluirUsuario() {
+    document.querySelector("#modalInfoUsuario").style.display = "none";
+    document.querySelector(".modal-excluir-usuario").style.display = "flex";
+}
+
+function exitModalExcluirUsuario() {
+    document.querySelector(".modal-excluir-usuario").style.display = "none";
+    document.querySelector("#modalInfoUsuario").style.display = "flex";
+}
+
+function exitModalsUsuario() {
+    document.querySelector(".modal-excluir-usuario").style.display = "none";
+    document.querySelector(".modal-editar").style.display = "none";
+    document.querySelector("#modalInfoUsuario").style.display = "none";
+    document.querySelector(".bg").style.display = "none";
+}
+
+function showModalCriarUsuario() {
+    document.querySelector(".bg").style.display = "flex";
+    document.querySelector(".modal-criar").style.display = "flex";
+}
+
+function exitModalCriarUsuario() {
+    document.querySelector(".bg").style.display = "none";
+    document.querySelector(".modal-criar").style.display = "none";
+}
+
 function deletarCampo() {
     if (document.getElementById("tipo-select-edit").value == "3") {
-        document.querySelector("#turma").style.display = "none";
+        document.querySelector("#turma").style.display = "flex";
         document.querySelector("#grupo").style.display = "flex";
         document.querySelector("#turma-aluno").style.display = "none";
         document.querySelector("#curso-aluno").style.display = "none";
-        document.querySelector("#curso").style.display = "flex";
+        document.querySelector("#grupo-aluno").style.display = "none";
+        document.querySelector("#curso").style.display = "none";
+        document.querySelector("#modal-editar").style.height = "55vh";
     } else if (document.getElementById("tipo-select-edit").value == "2") {
         document.querySelector("#turma-aluno").style.display = "flex";
         document.querySelector("#curso-aluno").style.display = "flex";
+        document.querySelector("#grupo-aluno").style.display = "flex";
         document.querySelector("#turma").style.display = "none";
         document.querySelector("#curso").style.display = "none";
         document.querySelector("#grupo").style.display = "none";
+        document.querySelector("#modal-editar").style.height = "65vh";
     } else {
-        document.querySelector("#turma").style.display = "flex";
+        document.querySelector("#turma").style.display = "none";
         document.querySelector("#grupo").style.display = "none";
         document.querySelector("#turma-aluno").style.display = "none";
         document.querySelector("#curso-aluno").style.display = "none";
-        document.querySelector("#curso").style.display = "flex";
+        document.querySelector("#grupo-aluno").style.display = "none";
+        document.querySelector("#curso").style.display = "none";
+        document.querySelector("#modal-editar").style.height = "50vh";
+    }
+}
+
+function alterarInputTipoUsuario() {
+    if (document.getElementById("tipo-select-criar").value == "3") {
+        document.querySelector("#turmaNovoUsuario").style.display = "flex";
+        document.querySelector("#grupoNovoUsuario").style.display = "flex";
+        document.querySelector("#turma-novo-aluno").style.display = "none";
+        document.querySelector("#curso-novo-aluno").style.display = "none";
+        document.querySelector("#grupo-novo-aluno").style.display = "none";
+        document.querySelector("#cursoNovoUsuario").style.display = "none";
+        document.querySelector("#modal-criar").style.height = "55vh";
+    } else if (document.getElementById("tipo-select-criar").value == "2") {
+        document.querySelector("#turma-novo-aluno").style.display = "flex";
+        document.querySelector("#curso-novo-aluno").style.display = "flex";
+        document.querySelector("#grupo-novo-aluno").style.display = "flex";
+        document.querySelector("#turmaNovoUsuario").style.display = "none";
+        document.querySelector("#cursoNovoUsuario").style.display = "none";
+        document.querySelector("#grupoNovoUsuario").style.display = "none";
+        document.querySelector("#modal-criar").style.height = "65vh";
+    } else {
+        document.querySelector("#turmaNovoUsuario").style.display = "none";
+        document.querySelector("#grupoNovoUsuario").style.display = "none";
+        document.querySelector("#turma-novo-aluno").style.display = "none";
+        document.querySelector("#curso-novo-aluno").style.display = "none";
+        document.querySelector("#grupo-novo-aluno").style.display = "none";
+        document.querySelector("#cursoNovoUsuario").style.display = "none";
+        document.querySelector("#modal-criar").style.height = "50vh";
     }
 }
