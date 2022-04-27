542925a124e09d59900f3b4f4ce60b914070e686
Ana150 <anaoliveira4267@gmail.com>
2022-04-27 09:11:21 -0300

    .

--- aluno/planner/script.js
+++ aluno/planner/script.js
@@ -10,3 +10,15 @@ tiposUsuario.forEach((tipoUsuario) => {
         tipoUsuario.classList.add("ativo");
     });
 });
+
+function showModal() {
+    document.querySelector(".modal-add-tarefa").style.display = "flex";
+}
+
+function exitModal() {
+    document.querySelector(".modal-add-tarefa").style.display = "none";
+}
+
+
+document.querySelector('.botao-adicionar').addEventListener("click", showModal)
+document.querySelector('.botao-adicionar').addEventListener("click", showModal)
