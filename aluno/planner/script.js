117668538d6ef7e62c2400aa728a6a1bc1774c2b
Ana150 <anaoliveira4267@gmail.com>
2022-04-29 11:26:59 -0300

    .

--- aluno/planner/script.js
+++ aluno/planner/script.js
@@ -12,3 +12,13 @@ tiposUsuario.forEach((tipoUsuario) => {
 });
 
 
+var kebab = document.querySelector('.kebab'),
+    middle = document.querySelector('.middle'),
+    cross = document.querySelector('.cross'),
+    dropdown = document.querySelector('.dropdown');
+
+kebab.addEventListener('click', function() {
+  middle.classList.toggle('active');
+  cross.classList.toggle('active');
+  dropdown.classList.toggle('active');
+})
