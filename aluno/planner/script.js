b6d4f283ed67247fd0603a610b28e78aa5753f76
Ana150 <anaoliveira4267@gmail.com>
2022-05-03 11:24:32 -0300

    menus planner prontos

--- aluno/planner/script.js
+++ aluno/planner/script.js
@@ -39,14 +39,14 @@ var childKebab2 = document.querySelector('.child-kebab2'),
     childCross2 = document.querySelector('.child-cross2'),
     childDropdown2 = document.querySelector('.child-dropdown2');
 
-    childKebab2.addEventListener('click', function() {
+    function open() {
     childMiddle2.classList.toggle('active');
     childCross2.classList.toggle('active');
     childDropdown2.classList.toggle('active');
-})
+}
 
 function xx(){
-    document.querySelector(".child-dropdown2").style.display = "flex";
+    document.querySelector(".child-dropdown").style.display = "flex";
 }
 
 function dxx(){
