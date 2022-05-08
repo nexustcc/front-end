9b26f030254811c519b239291e833a495f3ce42d
Artur Duarte <arturduartemoraes@gmail.com>
2022-05-07 23:12:56 -0300

    Integração de Professor

---
+++ professor/cursos/script.js
@@ -0,0 +1,121 @@
+"use strict";
+
+let localStorageUser = [];
+
+let nomeCurso;
+
+async function turmasCurso(idCurso) {
+    window.location.href = `turmas/index.html?idCurso=${idCurso}`;
+}
+
+function exibirDados(cursos) {
+    const container = document.getElementById("ul_container");
+
+    for (var i = 0; i < cursos.length; i++) {
+        const curso = document.createElement("li");
+        curso.classList = "list-group-item li";
+
+        nomeCurso = cursos[i].nome;
+
+        curso.innerHTML += `
+            <div class="curso">
+                <p class="curso">Curso:</p>
+                <img class="book" src="./img/icone_curso.svg" alt="book" />
+                <p class="nome" id="nomeCurso">${cursos[i].nome}</p>
+            </div>
+            <div class="button">
+                <a>
+                    <button class="terceiro_botao" class="btnEntrarCurso" onclick="turmasCurso(${cursos[i].idCurso})">
+                        TURMAS EXISTENTES
+                    </button>
+                </a>
+            </div>
+        `;
+
+        container.appendChild(curso);
+
+        console.log(cursos[i].idCurso);
+    }
+}
+
+const exibirDadosPesquisa = (cursos) => {
+    const htmlString = cursos
+        .map((cursos) => {
+            return `
+        <li class="list-group-item li">
+            <div class="curso">
+                <p class="curso">Curso:</p>
+                <img class="book" src="./img/icone_curso.svg" alt="book" />
+                <p class="nome" id="nomeCurso">${cursos.nome}</p>
+            </div>
+            <div class="button">
+                <a href="turmas/index.html">
+                    <button class="terceiro_botao" class="btnEntrarCurso" onclick="turmasCurso(${cursos.idCurso})">
+                        TURMAS EXISTENTES
+                    </button>
+                </a>
+            </div>
+        </li>`;
+        })
+        .join("");
+    document.getElementById("ul_container").innerHTML = htmlString;
+};
+
+const searchBar = document.getElementById("inputBuscarCurso");
+
+let cursos = [];
+searchBar.addEventListener("keyup", (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const cursosFiltrados = cursos.cursos.filter((curso) => {
+        return curso.nome.toLowerCase().includes(searchString);
+    });
+    exibirDadosPesquisa(cursosFiltrados);
+});
+
+async function getArrayCursos() {
+    const url = `http://localhost:3000/professor/listarCursos/${localStorageUser.idTipo}`;
+
+    fetch(url).then((response) => response.json);
+    const dados = await fetch(url);
+    cursos = await dados.json();
+    exibirDados(cursos.cursos);
+}
+
+const logout = () => {
+    localStorage.removeItem("user");
+    window.location.href = "../../home/login/";
+};
+
+const checkLogin = () => {
+    if (localStorage.user != undefined) {
+        localStorageUser = JSON.parse(localStorage.user);
+        if (localStorageUser.tipo == "professor") {
+            getArrayCursos();
+            document.getElementById("nomeProfessor").innerHTML =
+                localStorageUser.nome;
+        } else {
+            switch (localStorageUser.tipo) {
+                case "instituição":
+                    window.location.href = "../instituicao/";
+                    break;
+
+                case "aluno":
+                    window.location.href = "../../aluno/perfil/index.html";
+                    break;
+
+                case "avaliador":
+                    window.location.href = "../../home/index.html";
+                    alert("O acesso dos Avaliadores a plataforma é feito pelo APP");
+                    break;
+
+                default:
+                    window.location.href = "../home/index.html";
+            }
+        }
+    } else {
+        window.location.href = "../../home/login/index.html";
+    }
+};
+
+window.onload = checkLogin();
--- professor/cursos/turmas/scripts.js
+++ professor/cursos/turmas/scripts.js
@@ -1,5 +1,21 @@
 "use strict";
 
+let localStorageUser = [];
+
+let idCurso;
+
+let urlSplit = window.location.href.split(["?"])
+
+if(urlSplit[1] == '' || urlSplit[1] == undefined){
+    //window.location.href = '../index.html'
+
+    console.log(urlSplit)
+} else{
+    idCurso = urlSplit[1].split(["="])[1]
+}
+
+let nomeCurso;
+
 function showModal() {
     document.querySelector(".bg").style.display = "flex";
     document.querySelector(".modal").style.display = "flex";
@@ -77,3 +93,152 @@ const isInputNumber = (evt) => {
         evt.preventDefault();
     }
 };
+
+const exibirDadosTeste = (turmas) => {
+    const container = document.getElementById("turma-container");
+
+    if (turmas.length == 0) {
+        const sem_turmas = document.createElement("p");
+        sem_turmas.innerHTML = "NENHUMA TURMA CADASTRADA";
+        sem_turmas.style.textAlign = "center";
+        container.appendChild(sem_turmas);
+    } else {
+        for (var i = 0; i < turmas.length; i++) {
+            const turma = document.createElement("li");
+            turma.classList = "list-group-item li";
+
+            turma.innerHTML += `
+                    <div class="turma">
+                        <p class="turma">Turma:</p>
+                        <img class="book" src="./img/icone_turma.svg" alt="book">
+                        <p class="nome">${turmas[i].nome}</p>
+                    </div>
+                    <div class="button">
+                        <button onclick="showModalTurma(${turmas[i].idTurma})" class="terceiro_botao">SOBRE A TURMA</button>
+                        <button onclick="showModalGrupos(${turmas[i].idTurma})" class="primeiro_botao">GRUPOS DE TCC</button>
+                        <button onclick="showModalExcluir(${turmas[i].idTurma})" class="quarto_botao">EXCLUIR</button>
+                    </div>
+            `;
+
+            container.appendChild(turma);
+
+            const linha = document.createElement("img");
+            linha.src = "./img/linha.svg";
+
+            container.appendChild(linha);
+        }
+    }
+};
+
+const exibirDados = (turmas) => {
+    const container = document.getElementById("ul_container");
+
+    for (var i = 0; i < turmas.length; i++) {
+        const turma = document.createElement("li");
+        turma.classList = "list-group-item li";
+
+        turma.innerHTML += `
+            <div class="turma">
+                <p class="turma">Turma:</p>
+                <img class="book" src="./img/icone_turma.svg" alt="book">
+                <p class="nome" id="nomeTurmaPrincipal">${turmas[i].nome}</p>
+            </div>
+            <div class="button">
+                <button onclick="showModalTurma()" class="terceiro_botao" id="modalSobreATurma">SOBRE A
+                    TURMA</button>
+                <button onclick="showModalGrupos()" class="primeiro_botao" id="modalGruposTccTurma">GRUPOS DE
+                    TCC</button>
+            </div>
+        `;
+
+        console.log(turma);
+
+        container.appendChild(turma);
+    }
+}
+
+const exibirDadosPesquisa = (turmas) => {
+    const htmlString = turmas
+        .map((turmas) => {
+            return `
+        <li class="list-group-item li">
+            <div class="turma">
+                <p class="turma">Turma:</p>
+                <img class="book" src="./img/icone_turma.svg" alt="book">
+                <p class="nome" id="nomeTurmaPrincipal">${turmas.nome}</p>
+            </div>
+            <div class="button">
+                <button onclick="showModalTurma()" class="terceiro_botao" id="modalSobreATurma">SOBRE A
+                    TURMA</button>
+                <button onclick="showModalGrupos()" class="primeiro_botao" id="modalGruposTccTurma">GRUPOS DE
+                    TCC</button>
+            </div>
+        </li>`;
+        })
+        .join("");
+    document.getElementById("ul_container").innerHTML = htmlString;
+};
+
+const searchBar = document.getElementById("inputBuscarTurmas");
+
+let turmas = [];
+searchBar.addEventListener("keyup", (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const turmasFiltradas = turmas.turmas.filter((turma) => {
+        return turma.nome.toLowerCase().includes(searchString);
+    });
+    exibirDadosPesquisa(turmasFiltradas);
+
+});
+
+async function getArrayTurmas() {
+    if (idCurso == "" || idCurso == undefined) {
+        window.location.href = "../../index.html";
+    } else {
+        const url = `http://localhost:3000/turma/listarTurmasCurso/${idCurso}`;
+
+        fetch(url).then((response) => response.json);
+        const dados = await fetch(url);
+        let turmas = await dados.json();
+        exibirDados(turmas.turma);
+    }
+}
+
+const logout = () => {
+    localStorage.removeItem("user");
+    window.location.href = "../../home/login/";
+};
+
+const checkLogin = () => {
+    if (localStorage.user != undefined) {
+        localStorageUser = JSON.parse(localStorage.user);
+        if (localStorageUser.tipo == "professor") {
+            getArrayTurmas();
+            document.getElementById("nomeProfessor").innerHTML =
+                localStorageUser.nome;
+        } else {
+            switch (localStorageUser.tipo) {
+                case "instituição":
+                    window.location.href = "../../instituicao/";
+                    break;
+
+                case "aluno":
+                    window.location.href = "../../../aluno/perfil/index.html";
+                    break;
+
+                case "avaliador":
+                    window.location.href = "../../../home/index.html";
+                    alert("O acesso dos Avaliadores a plataforma é feito pelo APP");
+                    break;
+
+                default:
+                    window.location.href = "../../home/index.html";
+            }
+        }
+    } else {
+        window.location.href = "../../../home/login/index.html";
+    }
+};
+
+window.onload = checkLogin();
