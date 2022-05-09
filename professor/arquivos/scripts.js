5ca52724c541f3a19630303050f8cb8e2b3820f7
thiagoJoseB <79976791+cauamenezes@users.noreply.github.com>
2022-05-09 12:09:08 -0700

    fazendo tela de arquivos do professor

--- professor/arquivos/scripts.js
+++ professor/arquivos/scripts.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const imgAcoes = document.querySelector('.imgAcoes');
+const iconEditar = document.getElementById('editarPastaArquivo');
+const iconExcluir = document.getElementById('excluirPastaArquivo');
+
+imgAcoes.addEventListener('click', function(){
+
+    imgAcoes.style.display = 'none';
+    iconEditar.style.display = 'flex';
+    iconExcluir.style.display = 'flex';
+})
