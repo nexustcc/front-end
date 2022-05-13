5f74a5e441157d097adbbaa222abe32dc9960051
Artur Duarte <arturduartemoraes@gmail.com>
2022-05-13 10:28:22 -0300

    Alterando Link de Redirecionamento de Logout

--- aluno/planner/script.js
+++ aluno/planner/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let localStorageUser = [];
+
 let tiposUsuario = document.querySelectorAll(".tipo_usuario ul li");
 
 tiposUsuario.forEach((tipoUsuario) => {
@@ -84,3 +86,40 @@ function exitModalTarefaGeral() {
     document.querySelector(".modal-tarefa-geral").style.display = "none";
     document.querySelector(".bg").style.display = "none";
 }
+
+// const logout = () => {
+//     localStorage.removeItem("user");
+//     window.location.href = "../../home/login/";
+// };
+
+// const checkLogin = () => {
+//     if (localStorage.user != undefined) {
+//         localStorageUser = JSON.parse(localStorage.user);
+//         if (localStorageUser.tipo == "aluno") {
+//             getInfoAluno();
+//             document.getElementById("nomeAluno").innerHTML = localStorageUser.nome;
+//         } else {
+//             switch (localStorageUser.tipo) {
+//                 case "professor":
+//                     window.location.href = "../../professor/perfil/index.html";
+//                     break;
+
+//                 case "instituição":
+//                     window.location.href = "../../instituicao/index.html";
+//                     break;
+
+//                 case "avaliador":
+//                     window.location.href = "../../home/index.html";
+//                     alert("O acesso dos Avaliadores a plataforma é feito pelo APP");
+//                     break;
+
+//                 default:
+//                     window.location.href = "../home/index.html";
+//             }
+//         }
+//     } else {
+//         window.location.href = "../../home/index.html";
+//     }
+// };
+
+// window.onload = checkLogin();
