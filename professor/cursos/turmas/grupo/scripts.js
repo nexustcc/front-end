4ba8d83add2c1c91e306c74758f625daddc144d0
Mateus Bispo Ramos <niremalu.mateus@gmail.com>
2022-05-17 11:26:00 -0300

    Integração - Informações do grupo
    Co-Authored-By: Cauã Menezes <79976791+cauamenezes@users.noreply.github.com>

--- professor/cursos/turmas/grupo/scripts.js
+++ professor/cursos/turmas/grupo/scripts.js
@@ -1,5 +1,22 @@
 "use strict"
 
+let localStorageUser = [];
+
+
+let idGrupo;
+let urlSplit = window.location.href.split(["?"]);
+if (urlSplit[1] == "" || urlSplit[1] == undefined) {
+    window.location.href = "../../index.html";
+} else {
+    idGrupo = urlSplit[1].split(["="])[1];
+}
+
+
+
+
+
+
+
 const showModalEditarAluno = () =>{
     document.querySelector(".modal-editar-aluno").style.display = "flex";
     document.querySelector(".bg").style.display = "flex";
@@ -43,3 +60,112 @@ const exitModalExcluirUsuarioGrupo = () =>{
     document.querySelector(".modal-excluir-usuario-grupo").style.display = "none";
     document.querySelector(".bg").style.display = "none";
 }
+
+
+
+
+
+
+
+
+const converterDataBanco = (data) => {
+
+    if(data != null) {
+        let dataSlipt = data.split(["T"]);
+        let dataArray = dataSlipt[0];
+        let dataArraySplit = dataArray.split(["-"]);
+        let dataFinal = dataArraySplit[0] + "-" + dataArraySplit[1] + "-" + dataArraySplit[2];
+        return dataFinal;
+    }
+}
+
+const exibirGrupo = (infoGrupo, andamento) => {
+    let inputTema = document.getElementById('inputTemaProjetoGrupo');
+    let inputDescricaoProjeto = document.getElementById('textDescricaoProjetoGrupo');
+    let inputDataApresentacao = document.getElementById('inputDataApresentacaoProjetoGrupo');
+    let inputHoraApresentacaoProjetoGrupo = document.getElementById('inputHoraApresentacaoProjetoGrupo');
+    let barraProgressoProjeto = document.getElementById('barraProgressoProjeto');
+
+    inputDescricaoProjeto.value = infoGrupo.descricao
+    inputTema.value = infoGrupo.temaProjeto
+    inputDataApresentacao.value = converterDataBanco(infoGrupo.dataApresentacao)
+    inputHoraApresentacaoProjetoGrupo.value = infoGrupo.horaApresentacao
+    barraProgressoProjeto.innerHTML = andamento + '%';
+    barraProgressoProjeto.style.width = andamento + '%';
+
+}
+
+const exibirAlunos = (alunos) => {
+    const container = document.getElementById('listIntegrantesGrupo')
+
+    for (let a = 0; a < alunos.length; a++) {
+        
+        let aluno = document.createElement('li');
+
+        aluno.innerHTML += `
+            <button class="btnEditarAlunoGrupo" onclick="showModalEditarAluno(${alunos[a].idAluno})"><i class='bx bx-edit'></i></button>
+            <div class="integrantes">
+                <img src="http://localhost:3000/${alunos[a].foto}" class="iconify icon" style="color: #05204a" width="80" height="80">
+            </div>
+            <p id="nomeAlunoGrupo">${alunos[a].nome}</p>
+        `
+
+        container.appendChild(aluno)
+    }
+}
+
+const exibirProfessores = (professores) => {
+
+}
+
+
+
+const getInfoGrupo = async () => {
+    const url = `http://localhost:3000/grupo/informacoesGrupo/${idGrupo}`;
+
+    fetch(url).then((response) => response.json);
+    const dados = await fetch(url);
+    let grupo = await dados.json();
+    
+    exibirGrupo(grupo.grupo[0], grupo.andamento);
+    exibirAlunos(grupo.alunos)
+    exibirProfessores(grupo.professores)
+}
+
+
+const logout = () => {
+    localStorage.removeItem("user");
+    window.location.href = "../../../../home/login/";
+};
+
+const checkLogin = () => {
+    if (localStorage.user != undefined) {
+        localStorageUser = JSON.parse(localStorage.user);
+        if (localStorageUser.tipo == "professor") {
+            getInfoGrupo();
+            document.getElementById("nomeProfessor").innerHTML = localStorageUser.nome;
+        } else {
+            switch (localStorageUser.tipo) {
+                case "instituição":
+                    window.location.href = "../../../../instituicao/";
+                    break;
+
+                case "aluno":
+                    window.location.href = "../../../../aluno/perfil/";
+                    break;
+
+                case "avaliador":
+                    window.location.href = "../../../../home/";
+                    alert("O acesso dos Avaliadores a plataforma é feito pelo APP");
+                    break;
+
+                default:
+                    window.location.href = "../../../../home/";
+            }
+        }
+    } else {
+        window.location.href = "../../../../home/login/index.html";
+    }
+};
+
+window.onload = checkLogin();
