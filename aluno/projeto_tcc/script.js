628a70e87986d1327b241585fcc630c9320c86c9
Cauã Menezes <79976791+cauamenezes@users.noreply.github.com>
2022-05-21 15:34:31 -0300

    integrando grupo do aluno

--- aluno/projeto_tcc/script.js
+++ aluno/projeto_tcc/script.js
@@ -2,6 +2,104 @@
 
 let localStorageUser = [];
 
+let idGrupo;
+
+let urlSplit = window.location.href.split(["?"]);
+
+if (urlSplit[1] == "" || urlSplit[1] == undefined) {
+    window.location.href = "../perfil/";
+} else {
+    idGrupo = urlSplit[1].split(["="])[1];
+}
+
+
+
+const converterDataBanco = (data) => {
+
+    if(data != null) {
+        let dataSlipt = data.split(["T"]);
+        let dataArray = dataSlipt[0];
+        let dataArraySplit = dataArray.split(["-"]);
+        let dataFinal = dataArraySplit[0] + "-" + dataArraySplit[1] + "-" + dataArraySplit[2];
+        return dataFinal;
+    }
+}
+
+const exibirGrupo = (infoGrupo, andamento) => {
+
+    let inputTema = document.getElementById('inputTemaProjetoGrupo');
+    let inputDescricaoProjeto = document.getElementById('textDescricaoProjetoGrupo');
+    let inputDataApresentacao = document.getElementById('inputDataApresentacaoProjetoGrupo');
+    let inputHoraApresentacaoProjetoGrupo = document.getElementById('inputHoraApresentacaoProjetoGrupo');
+    let barraProgressoProjeto = document.getElementById('barraProgressoProjeto');
+
+    inputDescricaoProjeto.value = infoGrupo.descricao
+    inputTema.value = infoGrupo.temaProjeto
+    inputDataApresentacao.value = converterDataBanco(infoGrupo.dataApresentacao)
+    inputHoraApresentacaoProjetoGrupo.value = infoGrupo.horaApresentacao
+
+    barraProgressoProjeto.innerHTML = andamento + '%';
+
+    if(andamento > 3) {
+        barraProgressoProjeto.style.width = andamento + '%';
+    }    
+
+}
+
+const exibirAlunos = (alunos) => {
+    const containerAlunos = document.getElementById('listIntegrantesGrupo')
+
+    for (let a = 0; a < alunos.length; a++) {
+        
+        let aluno = document.createElement('li');
+        aluno.classList = 'list-group-item li-integrantes';
+
+        aluno.innerHTML += `
+        <div class="integrantes">
+            <img src="http://localhost:3000/${alunos[a].foto}" class="iconify icon" style="color: #05204a" width="80" height="80">
+        </div>
+        <p>${alunos[a].nome}</p>
+        `
+
+        containerAlunos.appendChild(aluno)
+    }
+}
+
+const exibirProfessores = (professores) => {
+    const containerProfessores = document.getElementById('listProfessoresGrupo');
+
+    for (let i = 0; i < professores.length; i++) {
+
+        let professor = document.createElement('li');
+        professor.classList = 'list-group-item li-orientadores';
+
+        professor.innerHTML += `
+        <div class="orientadores">
+            <img src="http://localhost:3000/${professores[i].foto}" class="iconify icon" style="color: #05204a" width="80" height="80">
+        </div>
+        <p>${professores[i].nome}</p>
+        `
+
+        containerProfessores.appendChild(professor);
+        
+    }
+
+}
+
+const getInfoGrupo = async () => {
+    const url = `http://localhost:3000/grupo/informacoesGrupo/${idGrupo}`;
+
+    fetch(url).then((response) => response.json);
+    const dados = await fetch(url);
+    let grupo = await dados.json();
+    
+    exibirGrupo(grupo.grupo[0], grupo.andamento);
+    exibirAlunos(grupo.alunos)
+    exibirProfessores(grupo.professores)
+}
+
+
+
 const logout = () => {
     localStorage.removeItem("user");
     window.location.href = "../../home/login/";
